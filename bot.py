c61ab772daf6080b1b7a9321c784fdffa6bbeb2a
cicerolbs <cicerolbs@gmail.com>
2025-04-12 18:48:09 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -234,4 +234,23 @@ def run():
 
 Thread(target=run).start()
 
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot EliteDigital está ativo!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
+
+
 bot.run(TOKEN)
